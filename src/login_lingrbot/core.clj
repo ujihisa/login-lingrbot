5efe07e11682e8a5be06299e86ff210465030a2d
ujihisa <ujihisa@gmail.com>
2013-04-06 17:55:21 -0700

    lingr-url func

--- src/login_lingrbot/core.clj
+++ src/login_lingrbot/core.clj
@@ -1,4 +1,10 @@
-(ns login-lingrbot.core)
+(ns login-lingrbot.core
+  (:require [clj-http.client :as client]))
+
+(defn lingr-url [room text bot-verifier]
+  (format
+    "http://lingr.com/api/room/say?room=%s&bot=login&text=%s&bot_verifier=%s"
+    room text bot-verifier))
 
 (defn -main []
   (prn (slurp "src/resources/lingr-post-url")))
