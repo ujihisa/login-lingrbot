e7a03dfc25de5e691bf3bec7ec48766c39427a4b
ujihisa <ujihisa@gmail.com>
2013-04-06 17:53:33 -0700

    uses clj-http

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://github.com/ujihisa/login-lingrbot"
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.0"]]
   :main login-lingrbot.core)
