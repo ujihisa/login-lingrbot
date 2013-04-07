0853bd65097bd61ba5b2a1106fa63e4c77309904
ujihisa <ujihisa@gmail.com>
2013-04-06 18:01:02 -0700

    test from clojure worked!

--- src/login_lingrbot/core.clj
+++ src/login_lingrbot/core.clj
@@ -1,12 +1,15 @@
 (ns login-lingrbot.core
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client])
+  (:import [java.net URLEncoder]))
 
-(defn lingr-url [room text bot-verifier]
+(defn make-lingr-url [room text bot-verifier]
   (format
     "http://lingr.com/api/room/say?room=%s&bot=login&text=%s&bot_verifier=%s"
     room text bot-verifier))
 
 (defn -main []
-  (if-let [bot-verifier (slurp "src/resources/bot-verifier")]
-    (prn 'bot-verifier bot-verifier)
+  (if-let [bot-verifier (clojure.string/trim-newline (slurp "src/resources/bot-verifier"))]
+    (let [msg (URLEncoder/encode "てすと from clojure")
+          lingr-url (make-lingr-url "computer_science" msg bot-verifier)]
+      (client/get lingr-url))
     (.out *err* "give me bot-verifier")))
